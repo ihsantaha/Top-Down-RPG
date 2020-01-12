fff6d99080d7c5112719b82df7b1f580eda6e42e
richardvo101 <richard.vocoding101@gmail.com>
2020-01-12 12:32:02 -0500

    Created main character and folders to hold assets.  Base scene is created

---
+++ Top_Down_Rpg/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D body;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
