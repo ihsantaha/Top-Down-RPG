dcb3ac86d3c8af63268552e76c3d027c8cfb8d46
Ihsan Taha <ihsan_taha@yahoo.com>
2020-01-12 22:29:16 -0500

    Merged ihsan and richard with splash screen and base screen

---
+++ TopDownRpg/Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public GameObject item;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMovement player = collision.GetComponent<PlayerMovement>(); 
+
+        if(player != null)
+        {
+            item = GameObject.Find("Potion");
+            Debug.Log("we have touched a character");
+            Destroy(item);
+        }
+    }
+}
