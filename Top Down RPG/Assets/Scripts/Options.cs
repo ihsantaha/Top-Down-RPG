0c03d2c8e8494090e6faec8faf10c84b5249e341
Ihsan Taha <ihsan_taha@yahoo.com>
2020-01-12 21:47:26 -0500

    Added scene loading functionality and splash screen

---
+++ Top Down RPG/Assets/Scripts/Options.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour {
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
+}
